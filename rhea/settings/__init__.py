2dff7d3d6c350c5d8be24b3ef4868de4ddc870cf
legua_25 <legua.2507@gmail.com>
2016-02-16 00:59:53 -0600

    Update: configured project and applied patch to neo4django import error. Remember to set the environment variables in the other PC and apply the patch. Then, work on the models and data input controllers ASAP. We must get this rolling by Wednesday, or else...

--- rhea/settings/__init__.py
+++ rhea/settings/__init__.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from _base import BaseConfiguration as Config
+from configurations.values import SecretValue
 from os.path import join
+
+"""
+
+	NOTE: There is a bug regarding neo4django for Django >= 1.9. The "django.utils.importlib" module was deprecated in
+	1.7 and removed in 1.9, making the import fail. Add the following to the file "neo4django/utils.py", line 242 to
+	create a workaround:
+
+	try: from django.utils.importlib import import_module
+	except ImportError: from importlib import import_module
+
+	Remember to post the fix as a pull request one of these days... 'cause that's what you do when you fix someone
+	else's laziness-triggered mistakes.
+
+"""
 
 class Development(Config):
 
@@ -10,6 +25,25 @@ class Development(Config):
 
 	# Security settings
 	SECRET_KEY = '2!j5*n=5^u+tlrq^d5c52ww*$*c7qgdv&2d*kv$=37rvh%nio6'
+
+	# Databases
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.mysql',
+			'HOST': 'localhost',
+			'NAME': SecretValue(environ_name = 'REL_NAME', environ_prefix = 'RHEA'),
+			'USER': SecretValue(environ_name = 'REL_USER', environ_prefix = 'RHEA'),
+			'PASSWORD': SecretValue(environ_name = 'REL_PASSWD', environ_prefix = 'RHEA')
+		}
+	}
+	NEO4J_DATABASES = {
+		'default': {
+			'HOST': 'localhost',
+			'PORT': 7474,
+			'ENDPOINT': SecretValue(environ_name = 'GRP_ENDPOINT', environ_prefix = 'RHEA')
+		}
+	}
+	DATABASE_ROUTERS = [ 'neo4django.utils.Neo4djangoIntegrationRouter' ]
 
 	# Static files
 	STATIC_ROOT = join(Config.BASE_DIR, 'static')
